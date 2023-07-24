063c6a5685f68c3c972abb0d14660c573d6eaad7
NatasaSQ <nt.zecevic@gmail.com>
2023-07-24 15:43:02 +0200

    base repository implementation

--- database/repositories/jobs.repository.ts
+++ database/repositories/jobs.repository.ts
@@ -1,30 +1,13 @@
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { Job, JobDocument } from '../schemas/job.schema';
-import { FilterQuery, Model } from 'mongoose';
+import { BaseRepository } from './base.repository';
 
 @Injectable()
-export class JobsRepository {
-  constructor(@InjectModel(Job.name) private jobModel: Model<JobDocument>) {}
-
-  async findOne(jobFilterQuery: FilterQuery<Job>): Promise<Job> {
-    return this.jobModel.findOne(jobFilterQuery);
-  }
-
-  async find(jobFilterQuery: FilterQuery<Job>): Promise<Job[]> {
-    return this.jobModel.find(jobFilterQuery);
-  }
-
-  async create(job: Job): Promise<Job> {
-    const newJob = new this.jobModel(job);
-    return newJob.save();
-  }
-
-  async findOneAndUpdate(
-    jobFilterQuery: FilterQuery<Job>,
-    job: Partial<Job>,
-  ): Promise<Job> {
-    return this.jobModel.findOneAndUpdate(jobFilterQuery, job);
+export class UserRepository extends BaseRepository<JobDocument> {
+  constructor(@InjectModel(Job.name) private jobModel: Model<JobDocument>) {
+    super(jobModel);
   }
 }
